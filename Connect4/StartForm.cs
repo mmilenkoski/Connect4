2f9678518a8f19fbceaa499341b7d2116fdda736
blagojce95 <blagojce95@gmail.com>
2016-05-14 20:01:59 +0200

    FinalAttempt

--- Connect4/StartForm.cs
+++ Connect4/StartForm.cs
@@ -22,6 +22,8 @@ namespace Connect4
             player2 = Color.Red;
             button1.BackColor = player1;
             button2.BackColor = player2;
+            difficulty = 3;
+            comboBox1.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -46,13 +48,15 @@ namespace Connect4
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Form1 form = new Form1((int)numericUpDown1.Value, (int)numericUpDown2.Value, player1, player2);
+            Form1 form = new Form1((int)numericUpDown1.Value, (int)numericUpDown2.Value, difficulty, player1, player2);
             this.Visible = false;
             DialogResult dr= form.ShowDialog();
             if(dr == DialogResult.Cancel)
             {
                 this.Visible = true;
             }
+            if (dr == DialogResult.Abort)
+                Close();
             
         }
 
@@ -64,6 +68,27 @@ namespace Connect4
             if (dr == DialogResult.Cancel)
             {
                 this.Visible = true;
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                difficulty = 3;
+            } else
+            {
+                difficulty = 6;
             }
         }
     }
